e691fa186848405f77e9b4fab2b7b5f5cb4b9640
Linh Nguyen <linhnguyen21@MacBook-Air-cua-Linh.local>
2025-12-10 15:40:26 +0700

    Fix: Use find_by(username:) instead of find_by_username

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::AuthController < ApplicationController
   def login
-    @user = User.find_by_username(params[:username])
+    @user = User.find_by(username: params[:username])
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       render json: { token: token, user: @user.as_json(except: :password_digest) }, status: :ok
